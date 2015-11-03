e63bd6f56e49f808a5e50c3811dfe0415406ae4d
Lars Michelsen <lm@larsmichelsen.com>
2015-11-03 20:15:03 +0100

    Updated missing file headers

--- update-file-headers.py
+++ update-file-headers.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 # encoding: utf-8
+#
+# pmatic - A simple API to to the Homematic CCU2
+# Copyright (C) 2015 Lars Michelsen <lm@larsmichelsen.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 import os
 import time
@@ -28,7 +45,8 @@ new_header = '''#!/usr/bin/env python
 
 repo_path = os.path.dirname(os.path.realpath(__file__))
 
-for f in glob.glob('%s/pmatic/*.py' % repo_path) \
+for f in glob.glob('%s/*.py' % repo_path) \
+       + glob.glob('%s/pmatic/*.py' % repo_path) \
        + glob.glob('%s/pmatic/*/*.py' % repo_path):
     new = [ new_header ]
 
