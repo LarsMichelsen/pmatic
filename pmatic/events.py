5ae81435ede5fb090d0b000ba791f8176a1dc35c
Lars Michelsen <lm@larsmichelsen.com>
2016-01-07 12:12:01 +0100

    Updated comments and some further tests with XML-RPC

--- pmatic/events.py
+++ pmatic/events.py
@@ -71,6 +71,7 @@ class EventListener(object):
 
     @classmethod
     def _next_id(cls):
+        """Each event listener gets a unique ID which is used to register with the CCU."""
         this_id = cls.ident
         cls.ident += 1
         return this_id
@@ -83,6 +84,7 @@ class EventListener(object):
 
 
     def _init_listen_address(self, listen_address):
+        """Parses the listen_address provided by the user."""
         if listen_address == None:
             # listen on all interfaces. Use port 9123 by default.
             self._listen_address = ('', 9123)
@@ -96,6 +98,7 @@ class EventListener(object):
 
 
     def _init_interface_id(self, interface_id):
+        """Initializes the interface ID of this object."""
         if interface_id != None:
             if utils.is_string(interface_id):
                 self._interface_id = interface_id
@@ -106,6 +109,12 @@ class EventListener(object):
 
 
     def init(self):
+        """Initializes this objects RPC server and registers with the CCU.
+
+        This method opens the XML-RPC server on the configured listen_addres and
+        sends an API call to the CCU to register the just started XML-RPC server.
+        The CCU is then sending XLM-RPC messages to this server.
+        """
         self._start_rpc_server()
         self._register_with_ccu()
 
@@ -113,6 +122,12 @@ class EventListener(object):
     def _start_rpc_server(self):
         """Starts listening for incoming XML-RPC messages from CCU."""
         self._server = EventXMLRPCServer(self._listen_address)
+        # Register system.listMethods, system.methodHelp and system.methodSignature
+        # FIXME: standard system.listMethods does not seem to be OK for the CCU. Seems as it
+        # is sending an argument with the call (I think the interace_id). Seems we need to
+        # build it on our own.
+        self._server.register_introspection_functions()
+        # Allow multicalls
         self._server.register_multicall_functions()
         self._server.register_instance(EventHandler(self.API, self))
         self._server.start()
@@ -143,6 +158,19 @@ class EventListener(object):
 
 
     def _find_listen_address_to_reach_ccu(self):
+        """Determines the host address to tell the CCU to send it's messages to.
+
+        When the listen_address is either not set or set to an empty string this makes
+        the XML-RPC server listen on all local interfaces / addresses. But the drawback
+        is that we don't know which URL we should tell the CCU to send it's events to.
+
+        This method determines the address to tell the CCU.
+
+        When the script is executed on the CCU it returns "127.0.0.1". Otherwise it creats
+        a socket and opens a connection to the CCU address (which is used by self.API)
+        and port 80. Then it knows which local IP address has been used to communicate
+        with the CCU. This address is then returned.
+        """
         if isinstance(self.API, LocalAPI):
             return "127.0.0.1"
 
@@ -191,6 +219,8 @@ class EventListener(object):
         return self._server.is_alive()
 
 
+import pprint
+
 
 class EventHandler(object):
     """Handles incoming XML-RPC calls."""
@@ -199,80 +229,76 @@ class EventHandler(object):
         self.listener = listener
 
 
-    # String   interface_id,   String   address,   String value_key, ValueType value
     # Mit dieser Methode teilt der Schnittstellenprozess der Logikschicht mit, dass sich ein
     # Wert geändert hat oder ein Event (z.B. Tastendruck) empfangen wurde.
     # Der  Parameter interface_id gibt  die  id  des  Schnittstellenprozesses  an,  der  das
     # Event sendet.
     # Der  Parameter address ist  die  Addresse  des  logischen  Gerätes,  zu  dem  der
-    # geänderte Wert / das Event gehört. 
-    # Der Parameter value_key ist der Name des entsprechenden Wertes. Die möglichen 
-    # Werte  für  value_key  ergeben  sich  aus  der  ParamsetDescription  des  entsprechenden 
-    # Parameter-Sets „VALUES“. 
-    # Der Parameter value gibt den neuen Wert bzw. den dem Event zugeordneten Wert an. 
-    # Der  Datentyp  von value ergibt  sich  aus  der  ParamsetDescription  des 
-    # Values-Parameter-Sets des entsprechenden logischen Gerätes. 
-    def event(self, *args):
-        print("EVENT", *args)
+    # geänderte Wert / das Event gehört.
+    # Der Parameter value_key ist der Name des entsprechenden Wertes. Die möglichen
+    # Werte  für  value_key  ergeben  sich  aus  der  ParamsetDescription  des  entsprechenden
+    # Parameter-Sets „VALUES“.
+    # Der Parameter value gibt den neuen Wert bzw. den dem Event zugeordneten Wert an.
+    # Der  Datentyp  von value ergibt  sich  aus  der  ParamsetDescription  des
+    # Values-Parameter-Sets des entsprechenden logischen Gerätes.
+    def event(self, interface_id, address, value_key, value):
+        print("EVENT", address, value_key, value)
+        return True
 
 
-    # Array<DeviceDescription> listDevices(String interface_id)
-    # Diese  Methode  gibt  alle  der  Logikschicht  bekannten 
-    # Geräte  für  den  Schnittstellen-prozess mit der Id interface_id
-    #  in Form von Gerätebeschreibungen zurück. Damit 
-    # kann  der  Schnittstellenprozess  durch  Aufruf  von  new
-    # Devices()  und  deleteDevices() einen Abgleich vornehmen. 
-    # Damit  das  funktioniert,  muss  sich  die  Logikschicht 
-    # diese  Informationen  zumindest teilweise merken. Es ist dabei ausreichend, wenn je
-    # weils die Member ADDRESS und VERSION einer DeviceDescription gesetzt sind. 
-    def listDevices(self, *args):
-        print("LIST_DEVICES", *args)
+    # Diese  Methode  gibt  alle  der  Logikschicht  bekannten Geräte für den Schnittstellen-Prozess
+    # mit der Id interface_id in Form von Gerätebeschreibungen zurück. Damit kann der
+    # Schnittstellenprozess durch Aufruf von  new Devices() und deleteDevices() einen Abgleich
+    # vornehmen. Damit  das  funktioniert,  muss  sich  die  Logikschicht diese  Informationen
+    # zumindest teilweise merken. Es ist dabei ausreichend, wenn je weils die Member ADDRESS
+    # und VERSION einer DeviceDescription gesetzt sind.
+    def listDevices(self, interface_id):
+        print("LIST_DEVICES")
         return []
 
 
-    # void newDevices(String interface_id , Array<DeviceDescription> dev_descriptions)
     # Mit  dieser  Methode  wird  der  Logikschicht  mitgeteilt,  dass  neue  Geräte  gefunden
     # wurden.
     # Der Parameter interface_id gibt die id des Schnittstellenprozesses an, zu dem das Gerät
     # gehört.
     # Der Parameter dev_descriptions ist ein Array, das die Beschreibungen der neuen Geräte enthält.
-    # Wenn dev_descriptions Geräte enthält, die der Logikschicht bereits beka nnt sind, 
-    # dann ist davon auszugehen, dass sich z.B. durch ein Firmwareupdate das Verhalten 
-    # des Gerätes geändert hat. Die Basisplatform muß dann einen Abgleich mit der neuen 
-    # Beschreibung  durchführen.  Dabei  sollte  die  Konfiguration  des  Gerätes  innerhalb  der 
-    # Logikschicht so weit wie möglich erhalten bleiben. 
-    def newDevices(self, *args):
-        print("NEW_DEVICES", *args)
+    # Wenn dev_descriptions Geräte enthält, die der Logikschicht bereits beka nnt sind,
+    # dann ist davon auszugehen, dass sich z.B. durch ein Firmwareupdate das Verhalten
+    # des Gerätes geändert hat. Die Basisplatform muß dann einen Abgleich mit der neuen
+    # Beschreibung  durchführen.  Dabei  sollte  die  Konfiguration  des  Gerätes  innerhalb  der
+    # Logikschicht so weit wie möglich erhalten bleiben.
+    def newDevices(self, interface_id, dev_descriptions):
+        pprint.pprint(("NEW_DEVICES", dev_descriptions))
+        return True
 
 
-    # void    deleteDevices(String    interface_id,    Array<String> addresses)
-    # Mit   dieser   Methode   wird   der   Logikschicht   mitgeteilt,   dass   Geräte   im 
+    # Mit   dieser   Methode   wird   der   Logikschicht   mitgeteilt,   dass   Geräte   im
     # Schnittstellenprozess gelöscht wurden.  Der Parameter interface_id
-    #  gibt die id des Schnittstellenprozesses an, zu dem das Gerät gehört. 
+    #  gibt die id des Schnittstellenprozesses an, zu dem das Gerät gehört.
     # Der  Parameter addresses ist  ein  Array,  das  die  Adressen  der
     # gelöschten  Geräte enthält.
-    def deleteDevices(self, *args):
-        print("DELETE_DEVICES", *args)
+    def deleteDevices(self, interface_id, addresses):
+        pprint.pprint(("DELETE_DEVICES", addresses))
+        return True
 
 
-    # void  updateDevice(String  interface_id,  String  address,  int hint)
-    # Mit dieser Methode wird der Logikschicht mitgeteilt, dass sich an einem Gerät etwas 
+    # Mit dieser Methode wird der Logikschicht mitgeteilt, dass sich an einem Gerät etwas
     # geändert hat.  Der Parameter interface_id gibt die id des Schnittstellenprozesses an, zu dem
-    # das Gerät gehört. 
-    # Der Parameter address ist die Adresse des Gerätes oder des Kanals, auf das sich die 
-    # Meldung bezieht. 
-    # Der Parameter hint spezifiziert die Änderung genauer: 
+    # das Gerät gehört.
+    # Der Parameter address ist die Adresse des Gerätes oder des Kanals, auf das sich die
+    # Meldung bezieht.
+    # Der Parameter hint spezifiziert die Änderung genauer:
     # • UPDATE_HINT_ALL=0
-    # Es hat eine nicht weiter spezifizierte Änderung stattgefunden und es sollen daher 
-    # alle möglichen Änderungen berücksichtigt werden. 
-    # •
-    # UPDATE_HINT_LINKS=1
+    # Es hat eine nicht weiter spezifizierte Änderung stattgefunden und es sollen daher
+    # alle möglichen Änderungen berücksichtigt werden.
+    # • UPDATE_HINT_LINKS=1
     # Es hat sich die Anzahl der Verknüpfungspartner geändert.
     #
-    # Derzeit  werden  nur  Änderungen  an  den  Verknüpfungspa rtnern  auf  diesem  Weg 
-    # mitgeteilt. 
-    def updateDevices(self, *args):
-        print("UPDATE_DEVICES", *args)
+    # Derzeit  werden  nur  Änderungen  an  den  Verknüpfungspa rtnern  auf  diesem  Weg
+    # mitgeteilt.
+    def updateDevices(self, interface_id, address, hint):
+        pprint.pprint(("UPDATE_DEVICES", address, hint))
+        return True
 
 
 
