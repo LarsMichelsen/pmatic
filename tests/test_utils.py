2ee4a212482d7e31dad8e7143f27263338fb884f
Lars Michelsen <lm@larsmichelsen.com>
2016-02-09 10:54:24 +0100

    manager: Added user friendly command line arguments

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -24,7 +24,9 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import pytest, sys
+import pytest
+import sys
+import os
 from pmatic import utils
 
 
